32994c204fbb923b288dbb4c6fd7d94045a85215
dimasaptr <dimasaptr@gmail.com>
2026-05-31 12:22:37 +0700

    Revert "Feature3/add to cart page"

--- backend/src/routes/promotion.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { getPromotions } from '../controllers/promotion.controller';
-
-const router = Router();
-
-router.get('/', getPromotions);
-
-export default router;
