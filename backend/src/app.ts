1c04fc8bf7bad57cfa8ec0674f03fecc51879e28
simon <simonpasaribu4@gmail.com>
2026-05-31 23:55:33 +0700

    fix: use default import for router in app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,7 +3,7 @@ import path from 'path'
 
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
-import { router } from './routes'
+import router from './routes'
 
 const app = express()
 
