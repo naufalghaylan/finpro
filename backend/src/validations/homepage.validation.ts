43886a7420e9c71a87772103ba3a9124a7cea889
naufalghaylan <naufalghaylan@gmail.com>
2026-07-09 22:05:25 +0700

    remove unused files

--- backend/src/validations/homepage.validation.ts
+++
@@ -1,6 +0,0 @@
-import { z } from 'zod';
-
-export const getHomepageQuerySchema = z.object({
-  lat: z.string().optional().refine(val => !val || !isNaN(Number(val)), { message: "Invalid latitude" }),
-  lng: z.string().optional().refine(val => !val || !isNaN(Number(val)), { message: "Invalid longitude" })
-});
